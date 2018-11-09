e42e4ab8f77d66f52c4bedacfaefff4e772ea348
Jeffreyvh <jeffreyvh@ymail.com>
2018-11-08 19:36:06 -0600

    added to htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,14 +2,14 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../client/home.html"));
+        res.sendFile(path.join(__dirname, "./client/home.html"));
       });
       
       app.get("/add", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../client/tables.html"));
+        res.sendFile(path.join(__dirname, "./client/tables.html"));
       });
       
       app.get("/all", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../client/reserve.html"));
+        res.sendFile(path.join(__dirname, "./client/reserve.html"));
       });
 };

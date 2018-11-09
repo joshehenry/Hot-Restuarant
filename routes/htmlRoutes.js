a2111f03e0ddfc7c3c324d5ba4ca32e7ac76897f
Jeffreyvh <jeffreyvh@ymail.com>
2018-11-08 19:30:41 -0600

    added to routes and server.js runs

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = function(app){
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "./../client/home.html"));
+      });
+      
+      app.get("/add", function(req, res) {
+        res.sendFile(path.join(__dirname, "./../client/tables.html"));
+      });
+      
+      app.get("/all", function(req, res) {
+        res.sendFile(path.join(__dirname, "./../client/reserve.html"));
+      });
+};

cb1c7a5b7f0b9ac1a52404bc07b705a8dfd2447e
Jeffreyvh <jeffreyvh@ymail.com>
2018-11-08 19:49:25 -0600

    corrected port number

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = function(app){
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "./client/home.html"));
+        res.sendFile(path.join(__dirname, "/client/home.html"));
       });
       
       app.get("/add", function(req, res) {
